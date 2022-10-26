9ab75620034fbcc897d366e9c319aba159785167
stephane <stephane@khedhiri.com>
2022-10-26 11:15:20 +0200

    first commit

--- client/src/store/film/film.thunk.js
+++ client/src/store/film/film.thunk.js
@@ -1,18 +1,23 @@
 
 import MovieDb, {MEDIA_TYPE, TIME_WINDOW} from "../../service/moviedb";
 import {actionRequestingData, actionReceivedDataMovie, actionReceivedDataTv} from './film.slice'
+import {actionAddError} from './../error/error.slice'
 
 export default  () => {
     return (dispatch, getState) => {
-        try{
-            dispatch(actionRequestingData())
-            MovieDb.trending(MEDIA_TYPE.MOVIE, TIME_WINDOW.DAY)
-                .then( datas => dispatch(actionReceivedDataMovie(datas.results)))
-            dispatch(actionRequestingData())
-            MovieDb.trending(MEDIA_TYPE.TV, TIME_WINDOW.DAY)
-                .then( datas => dispatch(actionReceivedDataTv(datas.results)))
-        }catch (e) {
 
-        }
+        dispatch(actionRequestingData())
+
+        MovieDb.trending(MEDIA_TYPE.MOVIE, TIME_WINDOW.DAY)
+            .then(datas => dispatch(actionReceivedDataMovie(datas.results)))
+            .catch(e => {
+                throw new Error(e.responseJSEON)
+            })
+
+        MovieDb.trending(MEDIA_TYPE.TV, TIME_WINDOW.DAY)
+            .then(datas => dispatch(actionReceivedDataTv(datas.results)))
+            .catch(e => {
+                throw new Error(e.responseJSEON)
+            })
     }
 }
